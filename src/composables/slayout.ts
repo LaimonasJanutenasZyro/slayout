47e2e253fd4cca403c1dd8fcc82647a834dedeb6
edvinasba <edvinas@zyro.com>
2022-01-10 16:28:14 +0200

    Pixel scan implementation

--- src/composables/slayout.ts
+++ src/composables/slayout.ts
@@ -1,4 +1,35 @@
 export const getGridBlocks = (initialData) => {
+  const { blocks, components } = initialData
+  const gridRowMap = {}
+  Object.entries(blocks).forEach(([blockId, blockData]) => {
+    // Sort by top so we can go top to bottom
+    const blockComponentsCopy = JSON.parse(JSON.stringify(blockData.components))
+    blockComponentsCopy.sort((a, b) => components[a].position.top - components[b].position.top)
+
+    let foundRowCounter = 0
+    let currentPixel = 0
+    let prevMatchedPixel = 0
+    const gridRowSizes = []
+
+    // Scan all pixels downwards
+    while (foundRowCounter < blockComponentsCopy.length * 2) {
+      blockComponentsCopy.forEach((componentId) => {
+        const { position } = components[componentId]
+        const { top, height } = position
+        const bottom = top + height
+
+        if (currentPixel === top || currentPixel === bottom) {
+          foundRowCounter += 1
+          gridRowSizes.push(currentPixel - prevMatchedPixel)
+
+          prevMatchedPixel = currentPixel
+        }
+      })
+      currentPixel += 1
+    }
+    gridRowMap[blockId] = gridRowSizes
+  })
   return {
+    gridRowMap,
   }
 }
--- test/slayout.test.ts
+++ test/slayout.test.ts
@@ -5,6 +5,7 @@ import renderData from '~/data/renderData'
 describe('getGridBlocks', () => {
   it('should correctly remap intial block data', () => {
     const blockData = getGridBlocks(initialData)
+    console.log(blockData) // only gets grid template rows
 
     expect(blockData).toEqual(renderData)
   })
